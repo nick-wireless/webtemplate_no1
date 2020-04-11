4dfa226aa9976bf02ba8a3c82bfdc51407bcc2c0
Nick Hanigan <australian.nick@gmail.com>
2020-04-11 13:42:21 +0930

    h2 & h3 Taglines
    Making visible.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,26 @@
 <template>
-  <Layout>
-    <div class="relative">
-      <g-image
-        class="absolute min-h-screen min-w-screen object-cover"
-        src="~/assets/images/home.jpg"
-      ></g-image>
-      <div
-        class="absolute bg-teal-900 opacity-75 min-h-screen min-w-full text-white"
-      ></div>
-    </div>
-    <h2 class=" font-display font-medium italic ">
-      {{ heading1 }}
-    </h2>
-    <h3 class=" font-serif font-headings font-semibold text-2xl h-8">
-      {{ subHeading }}
-    </h3>
-  </Layout>
+	<Layout>
+		<div id="background-cover">
+			<g-image
+				class="absolute min-h-screen min-w-screen object-cover z-10"
+				src="~/assets/images/home.jpg"
+			></g-image>
+			<div class="absolute bg-teal-900 opacity-75 min-h-screen min-w-full text-white z-20"></div>
+		</div>
+		<div class="h-56 relative"></div>
+		<div>
+			<h2
+				class=" inline-block relative z-30 mt-8 px-8 w-screen font-sans font-display text-white font-bold text-3xl tracking-wider text-right"
+			>
+				{{ heading1 }}
+			</h2>
+			<h3
+				class=" inline-block relative z-30 mt-8 px-8 w-screen font-sans font-display text-white font-semibold text-2xl  text-right leading-loose"
+			>
+				{{ subHeading }}
+			</h3>
+		</div>
+	</Layout>
 </template>
 
 <static-query>
@@ -28,18 +33,16 @@
 
 <script>
 export default {
-  metaInfo: {
-    title: "Home"
-  },
-  data() {
-    return {
-      heading1: "Leading with our mission, wherever that takes us.",
-      subHeading:
-        "Inputing a string that can be edited from time to time.  A template would likely have more words."
-    };
-  }
-};
+	metaInfo: {
+		title: 'Home'
+	},
+	data() {
+		return {
+			heading1: 'Our Mission Is Great',
+			subHeading: 'Let us tell you why. It begins with a great website.'
+		}
+	}
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  theme: {
-
-    extend: {
-      fontFamily: {
-        'display': ["IBM Plex Sans"],
-        'headings': ['Literata']
-      }
-    },
-  },
-  variants: {},
-  plugins: []
+	theme: {
+		extend: {
+			fontFamily: {
+				display: ['IBM Plex Sans'],
+				headings: ['Literata']
+			}
+		}
+	},
+	variants: {
+		backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+		fontStyle: ['responsive', 'hover', 'focus']
+	},
+	plugins: []
 }
